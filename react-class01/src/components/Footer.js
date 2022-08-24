bb9ac2f26053e1f902a6b31a7b044fdedf57bc1c
osamadado123 <osama.aldado@gmail.com>
2022-08-24 18:07:07 +0300

    done

---
+++ react-class01/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Footer extends React.Component {
+render (){
+    return (
+       <footer className="footer">
+            <p>&copy; Copy reserved Osama </p>
+        </footer>
+    )
+}  
+}
+export default Footer;
